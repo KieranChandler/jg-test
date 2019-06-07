ce4e52765e2d1a8ebc0b430f28bb95075203aafd
Kieran Chandler <bwoodyouth_14@hotmail.co.uk>
2019-06-07 15:16:53 +0100

    Implemented api endpoint in controller

--- JG.FinTechTest/GiftAid/GiftAidController.cs
+++ JG.FinTechTest/GiftAid/GiftAidController.cs
@@ -6,11 +6,21 @@ namespace JG.FinTechTest.GiftAid
     [ApiController]
     public class GiftAidController : ControllerBase
     {
+        private readonly GetGiftAidQuery _getGiftAidQuery;
+
+        public GiftAidController(GetGiftAidQuery getGiftAidQuery)
+        {
+            _getGiftAidQuery = getGiftAidQuery;
+        }
 
         [HttpGet]
-        public IActionResult Test()
+        public IActionResult Get([FromQuery] decimal amount)
         {
-            return Ok("Hello World");
+            return Ok(
+                _getGiftAidQuery.Query(new GetGiftAidQueryRequest
+                {
+                    PrincipalAmount = amount
+                }));
         }
     }
 }
